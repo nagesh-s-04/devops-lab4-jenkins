05a95593a3fd6f049f9026f359b1b5c3d2103040
nagesh-s-04 <vvce21cse0074@vvce.ac.in>
2024-07-10 15:29:09 +0530

    java file first change

--- Lab4.java
+++ Lab4.java
@@ -1,5 +1,6 @@
 public class Lab4{
     public static void main(String[] args){
         System.out.println("Hello World in Java");
+        System.out.println("First change in java file");
     }
 }
